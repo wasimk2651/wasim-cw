89df4d7e8fa696b2103bee6597059811e7705d0f
wasimk2651 <124280495+wasimk2651@users.noreply.github.com>
2023-03-30 10:46:26 +0100

    Create index.js
    index

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+console.log('byes')
+
+const port = 3000;
+const app = express();
+
+//app.set("view engine", "pug");
+
+//app.use(express.style("style"));
+
+console.log(process.env.NODE_ENV);
+
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST || "localhost",
+    user: "user",
+    password: "password",
+    database: "world",
+});
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.get("/cities", async (req, res) => {
+    const [rows, fields] = await db.execute("SELECT * FROM `city`")
+        return res.send(rows)
+});
+
+app.listen(port, () => {
+    console.log('server running');
+});
